4209c70ba7191493c6d0a97eff55c23e666a9066
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-19 18:37:29 +0500

    Remove `CipherDataEncoderInterface` and `CryptoTokenBufferInterface` and refactor `EncryptedStringInterface` for streamlined encoding and cryptographic token handling.

--- src/Encrypted/EncryptedStringInterface.php
+++ src/Encrypted/EncryptedStringInterface.php
@@ -8,17 +8,35 @@ declare(strict_types=1);
 
 namespace Charcoal\Contracts\Security\Encrypted;
 
-use Charcoal\Contracts\Buffers\Immutable\ImmutableBufferInterface;
-use Charcoal\Contracts\Security\Encoding\CipherDataEncoderInterface;
+use Charcoal\Contracts\Encoding\EncodingSchemeStaticInterface;
+use Charcoal\Contracts\Security\Cipher\CipherAlgorithmInterface;
 
 /**
  * Represents an encrypted string interface which extends the functionality of an immutable buffer.
  */
-interface EncryptedStringInterface extends ImmutableBufferInterface, CipherDataEncoderInterface
+interface EncryptedStringInterface
 {
-    public function iv(): CryptoTokenBufferInterface;
+    public function algo(): CipherAlgorithmInterface;
 
     public function kid(): string;
 
-    public function tag(): ?CryptoTokenBufferInterface;
+    public function iv(): string;
+
+    public function tag(): ?string;
+
+    public function ciphertext(): string;
+
+    public function encodeDto(
+        EncodingSchemeStaticInterface $encoding,
+        string                        $algo = "algo",
+        string                        $cipherText = "ciphertext",
+        string                        $iv = "iv",
+        ?string                       $tag = "tag",
+        ?string                       $keyRef = null
+    ): array;
+
+    public function encodeString(
+        EncodingSchemeStaticInterface $encoding,
+        string                        $tpl = "{algo}{iv}{tag}{cipherText}{keyRef}",
+    ): string;
 }
